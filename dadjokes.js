4a557909e86832b99cd99deb86594c5a858f8fa5
ckim14 <chikakim@gmail.com>
2022-04-16 15:09:37 -0400

    calls dadjoke API and appends to page

--- dadjokes.js
+++ dadjokes.js
@@ -1,20 +1,30 @@
-var repoList = document.querySelector("h3");
+var dadjokeoftheday = document.getElementById("dadjokeoftheday");
 var fetchButton = document.getElementById("fetch-button");
 
+var APIcalled = false;
+
 function getApi() {
-  var requestUrl = "GET https://icanhazdadjoke.com";
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      for (var i = 0; i < data.length; i++) {
-        var dadjoke = document.createElement("h3");
-        dadjoke.textContent = data[i].html_url;
-        dadjokeoftheday.appendChild(dadjoke);
-      }
+  //get dadjoke from API
+  if (APIcalled) {
+    return;
+  }
+
+  APIcalled = true;
+
+  fetch("https://icanhazdadjoke.com", {
+    headers: { Accept: "application/json" },
+  }).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+
+      //create H3 element
+      var showDadJoke = document.createElement("h3");
+      //set the text of the h3 element and p element
+      showDadJoke.innerText = data.joke;
+      //append the dynamically generated html to the page to the div associated with the id="dadjokeoftheday"
+      dadjokeoftheday.append(showDadJoke);
     });
-  alert("we did it!");
+  });
 }
 
 fetchButton.addEventListener("click", getApi);
